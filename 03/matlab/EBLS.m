3738bca6a9863c8e5490fc7d2a9bc6d2d412e5db
Ruochen Lin <ruochenlin@outlook.com>
2018-02-27 21:46:18 -0600

    modified ebls.m

--- 03/matlab/EBLS.m
+++ 03/matlab/EBLS.m
@@ -2,14 +2,14 @@ function [x, alpha] = EBLS(fun, x, d, alpha_start)
 c1 = 1e-3;
 c2 = 0.5;
 max_iter = 25;
+x.f = feval(fun, x.p, 1);
+x.g = feval(fun, x.p, 2);
 L = 0;
 U = Inf;
 alpha = alpha_start;
 iter = 1;
 while iter <= max_iter
-	x.f = feval(fun, x.p, 1);
-	x.g = feval(fun, x.p, 2);
-	step = -alpha * x.g;
+	step = alpha * d;
 	if feval(fun, x.p + step, 1) > x.f + c1 * x.g' * step
 		U = alpha;
 		alpha = (L + U) / 2;
