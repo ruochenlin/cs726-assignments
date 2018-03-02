c6137f92256e419e237cb095b8b06c94cbea6152
Ruochen Lin <ruochenlin@outlook.com>
2018-03-01 19:10:50 -0600

    Modified the position to apply eta

--- 03/matlab/SteepDescentBacktrack.m
+++ 03/matlab/SteepDescentBacktrack.m
@@ -7,18 +7,17 @@ x.g = feval(fun, x.p, 2);
 iter = 0;
 while (norm(x.g) > sdparams.toler) && (iter < sdparams.maxit)
 	d = -x.g;
-	if 0 ~= sdparams.eta
-		alpha = alpha * sdparams.eta;
-	else
-		alpha = 1;
-	end
-
 	while feval(fun, x.p + alpha * d, 1) > (x.f + c1 * x.g' * alpha * d)
 		alpha = beta * alpha;
 	end
 	x.p = x.p + alpha * d;
 	x.f = feval(fun, x.p, 1);
 	x.g = feval(fun, x.p, 2);
+	if 0 ~= sdparams.eta
+		alpha = alpha * sdparams.eta;
+	else
+		alpha = 1;
+	end
 	iter = iter + 1;
 end
 inform = struct('status', norm(x.g) <= sdparams.toler, 'iter', iter);
--- 03/matlab/SteepDescentLS.m
+++ 03/matlab/SteepDescentLS.m
@@ -6,13 +6,12 @@ alpha_start = 1;
 
 while (norm(x.g) > sdparams.toler) && (iter < sdparams.maxit)
 	d = -x.g;
+	[x, alpha_start] = EBLS(fun, x, d, alpha_start); % x.p, x.g, x.f updated
 	if  0. ~= sdparams.eta
 		alpha_start = alpha_start * sdparams.eta;
 	else
 		alpha_start = 1;
 	end
-	[x, alpha_start] = EBLS(fun, x, d, alpha_start); % x.p, x.g, x.f updated
-	% fprintf('%d, %.5f, %.10f\n', iter, alpha_start, x.f);
 	iter = iter + 1;
 end
 inform = struct('status', norm(x.g) <= sdparams.toler, 'iter', iter);
